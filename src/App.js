3e68b247c3b59e300b4d2d541a2815481fa93f1d
Veries Seals III <veries@me.com>
2023-04-08 17:28:25 -0500

    Added imports to App.js and Card.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
-
-/*
-Challenge: Build the Navbar component.
-Check the Figma file for the design specifics.
-*/
+import Card from "./components/Card"
 
 function App() {
     return (
         <div>
             <Navbar />
             <Hero />
+            <Card />
         </div>
     )
 }
---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+
+function Card() {
+    return (
+        <div className="card">
+            <img src="../images/katie-zaferes.png" className="card--image" />
+            <div className="card--stats">
+                <img src="../images/star.png" />
+                
+            </div>
+        </div>
+    )
+}
+
+export default Card;
