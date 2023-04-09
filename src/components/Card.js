9c076f6a68a40ae7f3f78fd67ab8679a04a2542e
Veries Seals III <veries@me.com>
2023-04-09 12:02:12 -0500

    Added styling to card and card logic

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,20 @@
 import React from "react"
+import Katie from "../components/img/katie-zaferes.png"
+import Star from "../components/img/star.png"
 
 
-function Card() {
+function Card(props) {
     return (
         <div className="card">
-            <img src="../images/katie-zaferes.png" className="card--image" />
+            <img src={Katie} className="card--image"/>
             <div className="card--stats">
-                <img src="../images/star.png" />
-                
+                <img src={Star} className="card--star"/>
+                <span>5.0</span>
+                <span className="grey">(6) • </span>
+                <span className="grey">USA</span>
             </div>
+            <p>Life Lessons with Katie Zaferes</p>
+            <p><span className="bold">From $136 </span> / person</p>
         </div>
     )
 }
